a35bec7ccc205fb17c20540604d87105a18700a6
huqiuyun <uidby@sina.com>
2013-11-13 22:19:09 +0800

    delete not class

--- hui/hframestyle_win32.cpp
+++
@@ -1,7 +0,0 @@
-
-bool HFrameStyle::winEvent( MSG *message, long *result )
-{
-    Q_UNUSED(message);
-    Q_UNUSED(result);
-    return false;
-}
--- hui/hgview_win32.cpp
+++
@@ -1,7 +0,0 @@
-
-bool HGView::winEvent(MSG *message, long *result)
-{
-    Q_D(HGView);
-
-    return d->mFrameStyle->winEvent( message, result );
-}
